e807a66f64f11e82d802e4093bad43578fdb11dc
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2020-05-04 11:22:01 -0500

    made a inventory class with a list of each soda object, made a method in inventory class that adds a set number of each soda to inventory upon instantiation

--- SodaMachine/CanSodaMachine.cs
+++ SodaMachine/CanSodaMachine.cs
@@ -9,7 +9,23 @@ namespace SodaMachine
     class CanSodaMachine
     {
         //member variables (has a)
+        public List<Coin> Coins;
+        public Inventory Inventory;
         //construtor (spawn)
+        public CanSodaMachine()
+        {
+            Inventory = new Inventory();
+        }
         //member methods (can do)
+
+        public void AcceptsCoinPayment()
+        {
+
+        }
+
+        public void ReturnsCoin() //add return type?? Coin ??
+        {
+           // return ;
+        }
     }
 }
---
+++ SodaMachine/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Inventory
+    {
+        public List<GrapeSoda> GrapeSodas;
+        public List<OrangeSoda> OrangeSodas;
+        public List<LemonSoda> LemonSodas;
+
+        public Inventory()
+        {
+            AddGrapeSoda();
+            AddOrageSoda();
+            AddLemonSoda();
+        }
+
+        public void AddGrapeSoda()
+        {
+            GrapeSodas = new List<GrapeSoda>();
+            for (int i = 0; i < 10; i++)
+            {
+                GrapeSoda grapeSoda = new GrapeSoda();
+                GrapeSodas.Add(grapeSoda);
+            }
+        }
+
+        public void AddOrageSoda()
+        {
+            OrangeSodas = new List<OrangeSoda>();
+            for (int i = 0; i < 6; i++)
+            {
+                OrangeSoda orangeSoda = new OrangeSoda();
+                OrangeSodas.Add(orangeSoda);
+            }
+        }
+
+        public void AddLemonSoda()
+        {
+            LemonSodas = new List<LemonSoda>();
+            for (int i = 0; i < 3; i++)
+            {
+                LemonSoda lemonSoda = new LemonSoda();
+                LemonSodas.Add(lemonSoda);
+            }
+        }
+    }
+}
